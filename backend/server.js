82d9ec2c3a86c4c653ca01bce6b63d48a6721097
Harkirat0 <amh2harkirat@gmail.com>
2023-04-20 11:20:27 -0700

    Bugs

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,6 @@ app.use("/api/users", userRouter);
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/frontend/build")));
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/build/index.html"));
 });
